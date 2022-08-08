156e084b5845a63a5378e38510db953a78abe14c
MitkaS7 <mitka.dev@gmail.com>
2022-08-08 12:53:47 +0300

    dev: done basic company searcher

--- src/components/bot.js
+++ src/components/bot.js
@@ -14,21 +14,38 @@ Rate.hasMany(Company)
 Company.hasMany(Rate)
 
 
-export default function startBot() {
 
+
+export default function startBot() {
   bot.on('message', async (msg) => {
     const text = msg.text;
-    const chatId = msg.chat.id;
+    const chatId = msg.chat.id; // вынести во вне
     if (text === '/start') {
       bot.sendMessage(chatId, "👋 Привет, что интересует?", mainOptions);
     }
   });
 
+  function searcHandler(msg) {
+    bot.onText(/.*/, async (msg) => {
+      const chatId = msg.chat.id; // вынести во вне
+      const company = await searchCompany(msg)
+        .then((company) => {
+          if (company) {
+            bot.sendMessage(chatId, createCompanyMarkup(company), repeatOptions);
+            bot.clearTextListeners();
+          } else {
+            bot.clearTextListeners();
+            bot.sendMessage(chatId, '😕 Такого клиента у нас нет.', repeatOptions)
+          }
+        })
+    });
+  }
+
   //
 
   bot.on('callback_query', async (msg) => {
     const data = msg.data;
-    const chatId = msg.message.chat.id;
+    const chatId = msg.message.chat.id; // вынести во вне
     // console.log(data);
     // console.log(chatId);
     if (data === 'back') {
@@ -36,30 +53,11 @@ export default function startBot() {
     };
     if (data === 'getсompany') {
       bot.sendMessage(chatId, 'Пришли название клиента в свободной форме');
-      bot.onText(/.*/, async (msg) => {
-        const company = await searchCompany(msg)
-          .then((company) => {
-            // console.log(company)
-            if (company) {
-              bot.sendMessage(chatId, createCompanyMarkup(company), repeatOptions);
-              bot.clearTextListeners();
-            } else {
-              bot.clearTextListeners();
-              bot.sendMessage(chatId, '😕 Такого клиента у нас нет.', repeatOptions)
-              searchCompany(msg)
-            }
-          })
-
-
-
-      });
-
-
-
+      searcHandler(msg);
     };
     if (data === 'newquerry') {
       bot.sendMessage(chatId, 'Пришли название клиента в свободной форме');
-
+      searcHandler(msg);
     }
   })
 
--- src/utils/companyMarkup.js
+++ src/utils/companyMarkup.js
@@ -1,5 +1,8 @@
 export default function createCompanyMarkup(companyData) {
+  if (!companyData.kpp) {
+    companyData.kpp = 'отсутствует'
+  }
   return `
-   <b>Клиент:</b> ${companyData.company_name} \n<b>ИНН:</b> <code>${companyData.inn}</code> \n<b>КПП:</b> <code>${companyData.kpp}</code> \n<b>Есть ИТС:</b> ${companyData.partnership}
+   \nНайден клиент: ✅ \n<b>${companyData.company_name}</b> \n<b>ИНН:</b> <code>${companyData.inn}</code> \n<b>КПП:</b> <code>${companyData.kpp}</code> \n<b>Есть ИТС:</b> ${companyData.partnership}
   `
 }

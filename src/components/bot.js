87334bcd2c9c93faa2014d2634c7dc04a7b78c8d
MitkaS7 <mitka.dev@gmail.com>
2022-08-15 10:32:32 +0300

    docs: add readme

--- src/components/bot.js
+++ src/components/bot.js
@@ -14,132 +14,37 @@ import {
   mainOptions,
   repeatOptions,
   rateOptions,
-  periodOptions,
+  itsPeriodOptions,
+  freshPeriodsOptions
 } from './keyboard.js'
 export const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 Rate.hasMany(Company)
 Company.hasMany(Rate)
 
-function rateSelectHandler() {
-  const rateRegExp = /ИТС/iu;
-  bot.onText(rateRegExp, msg => {
-    const { id } = msg.chat;
-    const rate = msg.text
-    console.log(rate)
-    return rate
-  })
-  // .then((selectedRate)=>{
-  //   console.log('SelectedRATE: ' + selectedRate)
-  //   return selectedRate
-  // })
-}
-
-const periodSelectHandler = (rate) => {
-  const periodRegExp = /\d{1,2}/;
-  console.log('rate is:' + rate)
-  bot.onText(periodRegExp, (msg, [monthQty]) => {
-
-  })
-  // bot.removeListener(regexp)
-  // return selectedRate
-}
 
 
-export default function startBot() {
+const rateSearchHandler = (rate,id) => {
+  bot.on('callback_query', query => {
+    const monthQty = query.data
+    // console.log(monthQty + 'RATE: ' + rate)
+    searchRate(rate, monthQty)
 
-  bot.on('message', msg => {
-    const { id } = msg.chat;
-    if (msg.text === 'Найти клиента') {
-      bot.sendMessage(id,'Пришли название клиента c большой буквы',{
-        reply_markup: {
-          remove_keyboard: true
-        }
+      .then((rates) => {
+        rates.forEach((rate) => {
+          bot.sendMessage(id, createRateMarkup(rate), { parse_mode: 'HTML' })
+            })
       })
-        .then(async () => {
-          searchCompanyHandler();
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    } else if (msg.text === 'Узнать цены') {
-      bot.sendMessage(id, 'Выбери категорию:', rateOptions);
-      // .then(async () => {
-      //   // rateSelectHandler()
-      // }).then(select => {
-      //   console.log('select: ' + select)
-      // })
-
-
-        // .then(async () => {
-
-        //   .then((selected) => {
-        //     console.log('selected: ' + selected)
-        //   })
-        // })
-        // .then(async (slctdRate) => {
-        //   periodSelectHandler(slctdRate)
-        // })
-
-            // .then((selectedRate) => {
-
-            //  searchRate(selectedRate, Number(monthQty))
-            //   .then((rates) => {
-                // rates.forEach((rate) => {
-                //   bot.sendMessage(id, createRateMarkup(rate), {parse_mode: 'HTML'})
-                    // .then(() => {
-                    //   bot.onText(/Перевыбрать тариф/, msg =>{
-                    //     bot.sendMessage(id, 'Выбери тариф:', rateOptions);
-
-                    //   })
-                    //   bot.onText(/Выйти в главное меню/, msg => {
-                    //     bot.sendMessage(id, 'Что нибудь еще?', mainOptions);
-                    //   })
-
-                    // })
-
-            // .then(() => {
-            //   bot.removeListener(periodRegExp);
-            // })
-
-      //     })
-      //   })
-      // })
-
-
-    } else if (msg.text === 'Получить контакты') {
-      console.log('Получить контакты')
-    } else if (msg.text === 'Назад') {
-      // bot.removeTextListener(/Назад/)
-      bot.sendMessage(id, 'Что интересует?', mainOptions);
-    } else {
-      return
+      .catch((error) => {
+        bot.sendMessage(id, debug(error))
+      })
+    // bot.answerCallbackQuery(query.id)
     }
-    return
-  })
+  )
 
-
- bot.onText(/start/, async msg => {
-   const { id } = msg.chat;
-   const userId = msg.from.id;
-  // const {username} = msg.from;
-  //  await User.create({ tg_id: userId, name: username })
-   auth(userId)
-   .then((isAllowed) => {
-     !isAllowed ? bot.sendMessage(id, `❌ Доступ запрещен ❌`) : bot.sendMessage(id, `👋 Привет, что интересует?`, mainOptions)
-   })
- });
-
- const repeatСlientSearch = () => {
-   bot.onText(/Найти еще/,  msg => {
-     const { id } = msg.chat;
-     bot.sendMessage(id, 'Пришли название клиента');
-     bot.removeTextListener(/Найти еще/);
-     searchCompanyHandler();
- })
 }
 
-  const searchCompanyHandler = () => {
+const companySearchHandler = () => {
     bot.onText(/.*/, async (msg) => {
       const chatId = msg.chat.id; // вынести во вне
       await searchCompany(msg)
@@ -157,6 +62,178 @@ export default function startBot() {
         })
     })
   }
+
+const repeatСlientSearch = () => {
+  bot.onText(/Найти еще/, msg => {
+    const { id } = msg.chat;
+    bot.sendMessage(id, 'Пришли название клиента');
+    bot.removeTextListener(/Найти еще/);
+    searchCompanyHandler();
+  })
+}
+
+
+export default function startBot() {
+
+  bot.onText(/start/, async msg => {
+    const { id } = msg.chat;
+    const userId = msg.from.id;
+    auth(userId)
+      .then((isAllowed) => {
+        !isAllowed ? bot.sendMessage(id, `❌ Доступ запрещен ❌`) : bot.sendMessage(id, `👋 Привет, что интересует?`, mainOptions)
+      })
+  });
+
+  bot.on('callback_query', async query => {
+    const { data } = query;
+    const { id } = query.message.chat;
+
+    // console.log(query)
+    switch (data) {
+// главное меню
+      case 'getprice':
+        bot.sendMessage(id, 'Выбери категорию:', rateOptions);
+        break;
+
+      case 'getсompany':
+        bot.sendMessage(id, 'Пришли название клиента c большой буквы');
+        companySearchHandler(data)
+
+        break;
+
+      // case 'getcontact':
+      //   bot.sendMessage(id, 'Чьи контакты интересуют?');
+      //   break;
+// назад и повтор
+      case 'back':
+        bot.sendMessage(id, 'Что интересует?', mainOptions);
+        break;
+
+      case 'newquerry':
+        bot.sendMessage(id, 'Пришли название клиента');
+        companySearchHandler(id);
+        break;
+// выбор тарифа
+      case 'itsbase':
+      case 'itsprof':
+        // rateSelectHandler()
+        bot.sendMessage(id, 'Выбери период:', itsPeriodOptions);
+        rateSearchHandler(data,id);
+        break;
+      case 'fnofd':
+        bot.sendMessage(id, 'тут пока пусто)')
+        break;
+      case 'fresh':
+        bot.sendMessage(id, 'Выбери период:', freshPeriodsOptions);
+        rateSearchHandler(data, id);
+        break;
+// навигация  в главное меню
+      case 'newchoice':
+        bot.sendMessage(id, 'Выбери тариф:', rateOptions);
+        break;
+      case 'mainmenu':
+        bot.sendMessage(id, 'Что интересует?', mainOptions);
+        break;
+
+      default:
+        break;
+    }
+  })
+
+
+//   bot.on('message', msg => {
+//     const { id } = msg.chat;
+//     if (msg.text === 'Найти клиента') {
+//       bot.sendMessage(id,'Пришли название клиента c большой буквы',{
+//         reply_markup: {
+//           remove_keyboard: true
+//         }
+//       })
+//         .then(async () => {
+//           searchCompanyHandler();
+//         })
+//         .catch((error) => {
+//           console.log(error)
+//         })
+//     } else if (msg.text === 'Узнать цены') {
+//       bot.sendMessage(id, 'Выбери категорию:', rateOptions);
+//       rateSelectHandler()
+//       // .then(async () => {
+//       //   // rateSelectHandler()
+//       // }).then(select => {
+//       //   console.log('select: ' + select)
+//       // })
+
+
+//         // .then(async () => {
+
+//         //   .then((selected) => {
+//         //     console.log('selected: ' + selected)
+//         //   })
+//         // })
+//         // .then(async (slctdRate) => {
+//         //   periodSelectHandler(slctdRate)
+//         // })
+
+//             // .then((selectedRate) => {
+
+//             //  searchRate(selectedRate, Number(monthQty))
+              // .then((rates) => {
+              //   rates.forEach((rate) => {
+              //     bot.sendMessage(id, createRateMarkup(rate), {parse_mode: 'HTML'})
+              //       .then(() => {
+              //         bot.onText(/Перевыбрать тариф/, msg =>{
+              //           bot.sendMessage(id, 'Выбери тариф:', rateOptions);
+
+              //         })
+              //         bot.onText(/Выйти в главное меню/, msg => {
+              //           bot.sendMessage(id, 'Что нибудь еще?', mainOptions);
+              //         })
+
+              //       })
+
+//             // .then(() => {
+//             //   bot.removeListener(periodRegExp);
+//             // })
+
+//       //     })
+//       //   })
+//       // })
+
+
+//     } else if (msg.text === 'Получить контакты') {
+//       console.log('Получить контакты')
+//     } else if (msg.text === 'Назад') {
+//       // bot.removeTextListener(/Назад/)
+//       bot.sendMessage(id, 'Что интересует?', mainOptions);
+//     } else {
+//       return
+//     }
+//     return
+//   })
+
+
+
+
+
+//   const searchCompanyHandler = () => {
+//     bot.onText(/.*/, async (msg) => {
+//       const chatId = msg.chat.id; // вынести во вне
+//       await searchCompany(msg)
+//         .then((company) => {
+//           if (company) {
+//             bot.sendMessage(chatId, createCompanyMarkup(company), repeatOptions);
+//           } else {
+//             bot.sendMessage(chatId, 'Такого клиента у нас нет. ❌', repeatOptions);
+//           }
+//           bot.removeTextListener(/.*/);
+//           repeatСlientSearch();
+//         })
+//         .catch((error) => {
+//           console.log(error)
+//         })
+//     })
+//   }
 
 
 }

cf6be3ff75b82547e84fa61fcb2ac5fac9661a61
MitkaS7 <mitka.dev@gmail.com>
2022-08-16 08:34:56 +0300

    dev: add rate search

--- src/utils/searchRate.js
+++ src/utils/searchRate.js
@@ -1,33 +1,36 @@
 import { Rate } from '../models/rate.js'
 import { Op } from 'sequelize'
 const categories = {
-  itsprof : 'КП ПРОФ',
-  itsbase : 'КП Базовый',
+  'КП ПРОФ' : 'КП ПРОФ',
+  'КП Базовый': 'КП Базовый',
   fresh : 'Фреш',
   fnofd : 'ФН и ОФД'
 }
 
-export const searchRate = async (rate, period) => {
+export const searchRate = async ({rateData}) => {
+  const {rate, period} = rateData
   let foundedRates = [];
-  if (Object.hasOwn(categories), rate){
-    const searchValue = categories[rate];
-    // console.log(searchValue)
-    await Rate.findAll({
-      where: {
-        title: {
-          [Op.substring]: searchValue
-        },
-        [Op.and]: [
-          {month_qty: period}
-        ]
+  await Rate.findAll({
+    where: {
+      title: {
+        [Op.substring]: rate
+      },
+      [Op.and]: [
+        { month_qty: period }
+      ]
+    }
+  })
+    .then(rates => {
+      if (!rates) {
+        console.log('rates not found')
+        return []
       }
+      foundedRates = rates;
     })
-      .then(rates => {
-        if (!rates) {
-          return []
-        }
-        foundedRates = rates;
-      })
-  }
+  // if (Object.hasOwn(categories), rate){
+  //   const searchValue = categories.rate;
+  //   console.log('searchValue: ' + searchValue)
+
+  // }
   return foundedRates;
 }
